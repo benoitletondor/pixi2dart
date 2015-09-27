177e8d5452077e20f58b519085ea4f1466698dfd
Benoit Letondor <benoit.letondor@gmail.com>
2015-09-27 13:40:03 +0200

    Finish sprite impl

--- lib/src/Sprite.dart
+++ lib/src/Sprite.dart
@@ -1,6 +1,9 @@
 part of pixi2dart;
 
-/// http://pixijs.github.io/docs/PIXI.Sprite.html
+/**
+ * The Sprite object is the base for all textured objects that are rendered to the screen
+ * http://pixijs.github.io/docs/PIXI.Sprite.html
+ */
 class Sprite extends Container {
   Sprite(Texture texture) : super(new JsObject(_PIXI['Sprite'], [texture._js]));
 
@@ -8,15 +11,38 @@ class Sprite extends Container {
   Sprite.fromImage(String imageId)
       : super(new JsObject(_PIXI['Texture']['fromImage'], [imageId]));
 
+  Sprite.fromFrame(String frameId, {bool crossorigin, SCALE_MODES scaleMode})
+      : super(new JsObject(_PIXI['Texture']['fromFrame'], [
+          frameId,
+          crossorigin ?? true,
+          scaleMode == null
+              ? _scaleModesValue(SCALE_MODES.DEFAULT)
+              : _scaleModesValue(scaleMode)
+        ]));
+
 // --------------------------------------->
 
   /// http://pixijs.github.io/docs/PIXI.Sprite.html#anchor
   set anchor(Point point) => _js["anchor"] = point._js;
   Point get anchor => new Point(_js["anchor"]);
 
-  // TODO : http://pixijs.github.io/docs/PIXI.Sprite.html#blendMode
-
   /// http://pixijs.github.io/docs/PIXI.Sprite.html#cachedTint
   set cachedTint(num value) => _js["cachedTint"] = value;
   num get cachedTint => _js["cachedTint"];
+
+  // TODO : http://pixijs.github.io/docs/PIXI.Sprite.html#shader
+
+  /// http://pixijs.github.io/docs/PIXI.Sprite.html#texture
+  set texture(Texture value) => _js["texture"] = value._js;
+  Texture get texture => new Texture(_js["texture"]);
+
+  /// http://pixijs.github.io/docs/PIXI.Sprite.html#tint
+  set tint(num value) => _js["tint"] = value;
+  num get tint => _js["tint"];
+
+// ----------------------------------------->
+
+  /// http://pixijs.github.io/docs/PIXI.Sprite.html#containsPoint
+  bool containsPoint(Point point) =>
+      _js.callMethod("containsPoint", [point._js]);
 }
